bfc8d283da136ddc374c11f92888541f4960b259
benjamincontrino <benjamin.contrino@gmail.com>
2026-01-20 17:22:06 -0500

    imports

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @import rvest
+#' @import httr  
+#' @import dplyr
+#' @import tidyr
+#' @import stringr
+#' @import ggplot2
+#' @import purrr
+#' @import readr
+#' @import tidymodels
+#' @import ranger
+#' @import DT
+NULL
